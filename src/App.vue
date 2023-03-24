984292d3f08f8751a77feb0e32c99c479a86845b
martinapiglio <martina.piglio@gmail.com>
2023-03-24 15:24:35 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+  import AppHeader from "./components/AppHeader.vue";
+
+  export default {
+        components: {
+            AppHeader
+        }
+    } 
+
+</script>
+
+<template>
+
+  <AppHeader></AppHeader>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,102 @@
+<script>
+    export default {
+  data() {
+    return {
+      links: [
+        'characters',
+        'comics',
+        'movies',
+        'tv',
+        'games',
+        'collectibles',
+        'videos',
+        'fans',
+        'news',
+        'shop'
+      ]
+    };
+  },
+};
+</script>
+
+<template>
+
+    <header>
+        <div class="container">
+
+            <div id="header-left">
+                <img src="img/dc-logo.png" alt="">
+            </div>
+
+            <div id="header-right">
+
+                <ul>
+                    <li v-for="link in links">
+                    {{ link }} </li>
+                </ul>
+
+            </div>
+
+        </div>
+    </header>
+
+</template>
+
+<style scoped lang="scss">
+
+    $main-color: #0282f9;
+
+    @mixin centerFlex() {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    @mixin centerMaxWidth() {
+        max-width: 980px;
+        margin: auto;
+    }
+    header .container {
+        @include centerFlex();
+        @include centerMaxWidth();
+        
+        height: 5rem;
+        
+        * {
+            @include centerFlex();
+            height: 100%;
+        }
+
+        #header-right {
+
+            ul {
+                gap: 1rem;
+                font-weight: bold;
+                font-size: 12px;
+                text-transform: uppercase;
+            };
+
+            li {
+                
+                border-bottom: 4px solid transparent;
+            }
+
+            li:hover {
+                border-bottom: 4px solid $main-color;
+                cursor: pointer;
+            }
+
+        } 
+
+
+    }
+
+
+
+
+
+
+
+
+</style>
