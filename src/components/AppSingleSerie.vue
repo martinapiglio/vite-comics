c9a515cf802e6b95342977c6e976d51380f14057
martinapiglio <martina.piglio@gmail.com>
2023-03-27 15:56:13 +0200

    cards: correct imgs added + styling

--- src/components/AppSingleSerie.vue
+++ src/components/AppSingleSerie.vue
@@ -33,17 +33,28 @@ export default{
     @use 'style/_mixins.scss' as *;
 
     .card {
-        width: calc(100% / 6 - (10px / 6 * 5));
+        width: calc(100% / 6 - (20px / 6 * 5));
         height: calc(100% / 2);
+
+        img {
+            width: 100%;
+            aspect-ratio: 1;
+            object-fit: cover;
+
+            cursor: pointer;
+        }
 
         #title {
             font-size: .8em;
             text-align: left;
             text-transform: uppercase;
+
+            transition: 1s;
         }
 
-        img {
-            width: 100%;
+        #title:hover {
+            text-decoration: underline;
+            cursor: pointer;
         }
     }
 
